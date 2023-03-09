8116a8c72a3da616049af300d1a78a190b9137fe
Đào Gia Hải <daogiahai@MacBook-Air-cua-Hai.local>
2023-03-09 10:26:45 +0700

    changing some styles for cart dropdown and category-item

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -2,7 +2,7 @@ import { createContext, useState } from "react";
 
 export const CartContext = createContext({
     isCartOpen: false,
-    setIsCartOpen: () => { }
+    setIsCartOpen: () => {}
 });
 
 export const CartProvider = ({ children }) => {

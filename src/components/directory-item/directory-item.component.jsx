96f619d7ba1c72f4ae9482abd457a185eb1b3e22
Gia Hai <haidao549@gmail.com>
2023-07-26 08:55:11 +0700

    adding navigate for home page

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,20 +1,30 @@
-import './directory-item.styles.jsx';
-import { BackgroundImage, Body, DirectoryItemContainer } from './directory-item.styles.jsx';
+import { Route, useNavigate } from "react-router-dom";
+import "./directory-item.styles.jsx";
+import {
+  BackgroundImage,
+  Body,
+  DirectoryItemContainer,
+} from "./directory-item.styles.jsx";
 
 const DirectoryItem = ({ category }) => {
-    const { title, imageUrl } = category;
-    return (
-        <DirectoryItemContainer>
-            <BackgroundImage
-                style={{
-                    backgroundImage: `url(${imageUrl})`
-                }} />
-            <Body>
-                <h2>{title}</h2>
-                <p>Shop Now</p>
-            </Body>
-        </DirectoryItemContainer>
-    );
+  const { title, imageUrl, route } = category;
+  const navigate = useNavigate();
+
+  const navigateHandler = () => navigate(route);
+
+  return (
+    <DirectoryItemContainer onClick={navigateHandler}>
+      <BackgroundImage
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <Body>
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+      </Body>
+    </DirectoryItemContainer>
+  );
 };
 
 export default DirectoryItem;

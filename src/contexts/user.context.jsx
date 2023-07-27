59607a356110e106aa5dbac98c13c80cdcab2c41
Gia Hai <haidao549@gmail.com>
2023-07-27 14:26:21 +0700

    convert user context into userReducer

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useEffect, useReducer } from "react";
 
 import {
   onAuthStateChangedListener,
@@ -10,8 +10,36 @@ export const UserContext = createContext({
   currentUser: null,
 });
 
+const USER_ACTION = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+
+const userReducer = (state, action) => {
+  console.log(state);
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTION.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload,
+      };
+    default:
+      throw new Error(`Unhandle type ${type} in userReducer`);
+  }
+};
+
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
+  const [state, dispatch] = useReducer(userReducer, INITIAL_STATE);
+  const { currentUser } = state;
+  const setCurrentUser = (user) => {
+    dispatch({ type: "SET_CURRENT_USER", payload: user });
+  };
+
   const value = { currentUser, setCurrentUser };
 
   useEffect(() => {

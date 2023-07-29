371d0c8fcb7c3b7d47daded7ffff3fc415066f1f
Gia Hai <haidao549@gmail.com>
2023-07-29 10:13:50 +0700

    optimize categories selector

--- src/store/categories/categories.selector.js
+++ src/store/categories/categories.selector.js
@@ -1,6 +1,18 @@
-export const selectCategoriesMap = (state) => {
-  return state.categories.categoriesArray.reduce((acc, curr) => {
-    const { title, items } = curr;
-    return { ...acc, [title.toLowerCase()]: items };
-  }, {});
-};
+import { createSelector } from 'reselect';
+
+const selectCategoriesReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+  [selectCategoriesReducer],
+  (categories) => categories.categoriesArray
+);
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categoriesArray) => {
+    return categoriesArray.reduce((acc, curr) => {
+      const { title, items } = curr;
+      return { ...acc, [title.toLowerCase()]: items };
+    }, {});
+  }
+);
